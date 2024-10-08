45aaa92ae86f8cb184192bd54bd0ed2f8e6a9305
dmora01 <dmora1496@gmail.com>
2024-10-07 18:20:40 -0700

    feat: add support for hash comparisons

--- Sources/fsff/cli.swift
+++ Sources/fsff/cli.swift
@@ -20,38 +20,68 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-import Foundation
 import ArgumentParser
-
+import Foundation
 import libfsff
 
 @main
 struct fsff: ParsableCommand {
-    @Argument(help: "The folder/file to process")
-    var path: String = ""
-
-    @Option(name: .shortAndLong, help: "The mode to run in.")
+    @Argument(help: "The mode to run in.")
     var mode: Mode
+
+    @Option(name: .shortAndLong, help: "The first target file")
+    var oneTarget: String = ""
+
+    @Option(name: .shortAndLong, help: "The second target file. Use with compare mode")
+    var twoTarget: String? = nil
 
     mutating func run() throws {
         let fileManager: FileManager = FileManager.default
-        let filePath: URL = URL(fileURLWithPath: path)
+        let target1Path: URL = URL(fileURLWithPath: oneTarget)
 
-        if !fileManager.fileExists(atPath: filePath.path) {
-            print("Does not exist: \(filePath)")
+        // Ensure that the target file exists
+        if !fileManager.fileExists(atPath: target1Path.path) {
+            print("Does not exist: \(target1Path.path)")
             throw ArgumentParser.ExitCode.failure
+        }
+        // Verifiy what mode is selected
+        if mode == .compare {
+            // Ensure that the second target file exists
+            if twoTarget == nil {
+                print("Missing target2")
+                throw ArgumentParser.ExitCode.failure
+            }
+            if !fileManager.fileExists(atPath: twoTarget!) {
+                print("Does not exist: \(twoTarget!)")
+                throw ArgumentParser.ExitCode.failure
+            }
         }
 
         switch mode {
-            case .hash:
-                let results: [String] = getFileHashes(from: filePath)
-                print(results.joined(separator: "\n"))
-                break
+        // Run the hash command
+        case .hash:
+            let results: [String] = getFileHashes(from: target1Path)
+            print(results.joined(separator: "\n"))
+            break
 
-            default:
-                print("Invalid option")
+        // Run the compare command
+        case .compare:
+            let isSameHash: Bool = compareHashes(
+                target1: target1Path,
+                target2: URL(fileURLWithPath: twoTarget!)
+            )
+
+            if !isSameHash {
+                print("Hashes do not match.")
                 throw ArgumentParser.ExitCode.failure
-                
+            } else {
+                print("Hashes match.")
+            }
+            break
+
+        default:
+            print("Invalid option")
+            throw ArgumentParser.ExitCode.failure
         }
     }
 }

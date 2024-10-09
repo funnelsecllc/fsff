a9e239426c2399314af124aa364a885b8a4c2a86
dmora01 <dmora1496@gmail.com>
2024-10-08 20:30:07 -0700

    feat: add enum for selecting the key size

---
+++ Sources/libfsff/Constants/Size.swift
@@ -0,0 +1,31 @@
+// MIT License
+
+// Copyright (c) 2024 FunnelSec LLC
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import ArgumentParser
+import Foundation
+
+/// Enum for controlling what key size to create in the cli
+public enum Size: String, ExpressibleByArgument, CaseIterable {
+    case bits128 = "bits128"
+    case bits192 = "bits192"
+    case bits256 = "bits256"
+}
